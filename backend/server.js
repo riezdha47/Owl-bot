8aead8412d95976bd134369e5099ca9337429876
riezdha47 <riezdha47@gmail.com>
2026-05-14 05:22:16 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-server.js
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Backend aktif')
+})
+
+app.listen(3000, () => {
+  console.log('Backend jalan di port 3000')
+})
