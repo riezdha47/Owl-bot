4889adb89cb220ff447d1774344fd3888f95554d
riezdha47 <riezdha47@gmail.com>
2026-05-14 06:32:56 +0700

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,13 @@
-
+exports.discordLogin = async(req, res) => {
+    try {
+        res.status(200).json({
+            status: true,
+            message: 'Discord OAuth Ready'
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+};
