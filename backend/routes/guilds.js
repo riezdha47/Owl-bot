a87904703f4e73a7e523b641fa6539a49b83228b
riezdha47 <riezdha47@gmail.com>
2026-05-14 06:22:48 +0700

    Update guilds.js

--- backend/routes/guilds.js
+++ backend/routes/guilds.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const router = express.Router();
+
+router.get('/', async(req, res) => {
+    res.status(200).json({
+        status: true,
+        data: []
+    });
+});
+
+module.exports = router;
