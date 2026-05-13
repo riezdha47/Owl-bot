1127fd9b6c2039630a65f5e8ca79b959eb7ccfb2
riezdha47 <riezdha47@gmail.com>
2026-05-14 06:27:04 +0700

    Update guildController.js

--- backend/controllers/guildController.js
+++ backend/controllers/guildController.js
@@ -1,1 +1,13 @@
-
+exports.getGuilds = async(req, res) => {
+    try {
+        res.status(200).json({
+            status: true,
+            data: []
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+};
