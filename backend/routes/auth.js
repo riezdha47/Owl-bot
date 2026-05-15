b633ccad323a5efdf44d87aa59e2446c87fa111f
riezdha47 <riezdha47@gmail.com>
2026-05-15 07:53:50 +0700

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,11 @@
+const express = require('express');
 
+const router = express.Router();
+
+const {
+    discordLogin
+} = require('../controllers/authController');
+
+router.get('/discord', discordLogin);
+
+module.exports = router;
