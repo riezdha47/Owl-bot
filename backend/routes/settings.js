fdaeb13b248d8ab7a04ead25bbda8deb08390dfc
riezdha47 <riezdha47@gmail.com>
2026-05-15 07:49:30 +0700

    Update settings.js

--- backend/routes/settings.js
+++ backend/routes/settings.js
@@ -1,1 +1,14 @@
+const express = require('express');
 
+const router = express.Router();
+
+const {
+    getSettings,
+    updateSettings
+} = require('../controllers/settingsController');
+
+router.get('/', getSettings);
+
+router.post('/', updateSettings);
+
+module.exports = router;
