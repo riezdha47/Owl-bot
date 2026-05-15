ca61885ba26ad4382353e3a469463d3490c18e11
riezdha47 <riezdha47@gmail.com>
2026-05-15 19:05:55 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 const app = require('./server');
 
+const {
+    initializeDatabase
+} = require('./services/databaseService');
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const startBackend = async() => {
+    try {
+        await initializeDatabase();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startBackend();
