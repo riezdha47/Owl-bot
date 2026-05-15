6a424eb9a3aaad3df50057dc77b7c9bd2c3ccfa4
riezdha47 <riezdha47@gmail.com>
2026-05-15 08:25:29 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 
 const guildRoutes = require('./routes/guilds');
+const settingsRoutes = require('./routes/settings');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/guilds', guildRoutes);
+app.use('/settings', settingsRoutes);
+app.use('/auth', authRoutes);
 
 app.get('/', async(req, res) => {
     res.status(200).json({
