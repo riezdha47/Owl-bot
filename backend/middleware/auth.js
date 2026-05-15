1a0022cf68d70344fba4790b0d500b8c6885f346
riezdha47 <riezdha47@gmail.com>
2026-05-15 08:20:19 +0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,10 @@
-
+module.exports = async(req, res, next) => {
+    try {
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            status: false,
+            message: 'Unauthorized'
+        });
+    }
+};
