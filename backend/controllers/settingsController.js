94667924ee453f4819912e68a4e4b8e105809fe2
riezdha47 <riezdha47@gmail.com>
2026-05-15 19:03:14 +0700

    Update settingsController.js

--- backend/controllers/settingsController.js
+++ backend/controllers/settingsController.js
@@ -1,8 +1,16 @@
+const GuildSettings = require('../database/models/GuildSettings');
+
 exports.getSettings = async(req, res) => {
     try {
+        const { guildId } = req.query;
+
+        const settings = await GuildSettings.findOne({
+            guildId
+        });
+
         res.status(200).json({
             status: true,
-            data: {}
+            data: settings
         });
     } catch (error) {
         res.status(500).json({
@@ -14,6 +22,24 @@ exports.getSettings = async(req, res) => {
 
 exports.updateSettings = async(req, res) => {
     try {
+        const { guildId, data } = req.body;
+
+        let settings = await GuildSettings.findOne({
+            guildId
+        });
+
+        if (!settings) {
+            settings = await GuildSettings.create({
+                guildId,
+                ...data
+            });
+        } else {
+            await GuildSettings.updateOne(
+                { guildId },
+                data
+            );
+        }
+
         res.status(200).json({
             status: true,
             message: 'Settings updated successfully'
