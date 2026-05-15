2334a95fef83beea82ff679769cb1b7d4552486d
riezdha47 <riezdha47@gmail.com>
2026-05-15 08:18:47 +0700

    Update discordService.js

--- backend/services/discordService.js
+++ backend/services/discordService.js
@@ -1,1 +1,16 @@
+exports.getBotGuilds = async(client) => {
+    try {
+        const guilds = client.guilds.cache.map(guild => ({
+            id: guild.id,
+            name: guild.name,
+            icon: guild.iconURL(),
+            members: guild.memberCount
+        }));
 
+        return guilds;
+    } catch (error) {
+        console.log(error);
+
+        return [];
+    }
+};
