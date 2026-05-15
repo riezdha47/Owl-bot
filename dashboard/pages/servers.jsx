51f643c3a87724d11941c7d8634b46a9a0ebb71b
riezdha47 <riezdha47@gmail.com>
2026-05-15 18:43:46 +0700

    Update servers.jsx

--- dashboard/pages/servers.jsx
+++ dashboard/pages/servers.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import api from '../services/api';
+
 export default function Servers() {
+    const [servers, setServers] = useState([]);
+
+    useEffect(() => {
+        fetchServers();
+    }, []);
+
+    const fetchServers = async() => {
+        try {
+            const response = await api.get('/guilds');
+
+            setServers(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div>
             <h1>Servers Page</h1>
+
+            {
+                servers.map(server => (
+                    <div key={server.id}>
+                        {server.name}
+                    </div>
+                ))
+            }
         </div>
     );
 }
