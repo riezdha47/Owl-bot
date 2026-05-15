f1409354b49a788cb7cc051ca7c58a07133f0537
riezdha47 <riezdha47@gmail.com>
2026-05-15 08:01:43 +0700

    Update mongo.js

--- backend/database/mongo.js
+++ backend/database/mongo.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
 
+const connectMongoDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = connectMongoDB;
