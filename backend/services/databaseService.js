cf630beb34bb8ce21a887515ac816431f3e002f4
riezdha47 <riezdha47@gmail.com>
2026-05-15 08:19:26 +0700

    Update databaseService.js

--- backend/services/databaseService.js
+++ backend/services/databaseService.js
@@ -1,1 +1,11 @@
+const connectMongoDB = require('../database/mongo');
 
+exports.initializeDatabase = async() => {
+    try {
+        await connectMongoDB();
+
+        console.log('Database initialized');
+    } catch (error) {
+        console.log(error);
+    }
+};
